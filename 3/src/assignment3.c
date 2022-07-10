1d28fe283e504b63969828c4c8a41c46e835e339
Zixuan Fan <ge43yeb@mytum.de>
2022-07-10 21:55:05 +0200

    sfdsdf

--- 3/src/assignment3.c
+++ 3/src/assignment3.c
@@ -199,7 +199,7 @@ void run(int fd, const char *ipaddr, int timeoutval, int attempts,
 		  }
 		}
 		printf("\n"); //ending of the current hop
-		if (out){fprint(stderr, "out\n");break;}
+		if (out){fprintf(stderr, "out\n");break;}
 	}
 
 /*===========================================================================*/
